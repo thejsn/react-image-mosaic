a9359bf1847a0a5d01aeee62c83a1549d52f6e67
thejsn <thejsn@users.noreply.github.com>
2017-05-14 15:56:05 +0200

    Added demo.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,64 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom'
+import ReactImageMosaic from '../../src/index';
 
-import Component from '../../src'
+import Images from '../assets/images.json';
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-image-mosaic Demo</h1>
-      <Component/>
-    </div>
-  }
-})
+const STATIC = '/assets/';
+
+export default class Demo extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.loadProgressChanged = this.loadProgressChanged.bind(this);
+		this.clickedCanvas = this.clickedCanvas.bind(this);
+		this.state = {
+			loadProgress: 0,
+			target: null
+		}
+	}
+
+	componentDidMount() {
+		this.setState({
+			target: STATIC + 'images/' + Images[8].name
+		});
+	}
+	
+	clickedCanvas(data) {
+		this.setState({
+			target: data.image
+		});
+	}
+
+	loadProgressChanged(progress) {
+		this.setState({
+			loadProgress: progress
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>react-image-mosaic Demo</h1>
+
+				{ this.state.loadProgress < 1 ? (
+				<pre>
+					Loading { Images.length } images...
+					({ Math.round(this.state.loadProgress * 100) }%)
+				</pre>
+				) : null }
+
+				<ReactImageMosaic
+					onClick={ this.clickedCanvas }
+					onLoadProgress={ this.loadProgressChanged }
+					colorBlending={ 0.6 }
+					width={ 800 }
+					height={ 800 }
+					sources={ Images.map(img => STATIC + 'images/' + img.name) } 
+					target={ this.state.target }/>
+			</div>
+		);
+	}
+}
 
 render(<Demo/>, document.querySelector('#demo'))
