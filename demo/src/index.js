bd6038e7b4f94e1310c803a7d76a65807f7d5c12
thejsn <thejsn@users.noreply.github.com>
2017-09-24 12:46:58 +0200

    Made demo page a bit nicer.

--- demo/src/index.js
+++ demo/src/index.js
@@ -21,7 +21,7 @@ export default class Demo extends Component {
 
 	componentDidMount() {
 		this.setState({
-			target: STATIC + 'images/' + Images[8].name
+			target: STATIC + 'images/' + Images[4].name
 		});
 	}
 	
@@ -37,10 +37,20 @@ export default class Demo extends Component {
 		});
 	}
 
+	getImgSrc(img) {
+		return typeof img === 'string' ?
+			img : 
+			img.src;
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>react-image-mosaic Demo</h1>
+			<div className="demo-container">
+				<h1>react-image-mosaic</h1>
+
+				<p>
+					<a href="https://github.com/thejsn/react-image-mosaic">Source on GitHub</a>
+				</p>
 
 				{ this.state.loadProgress < 1 ? (
 				<pre>
@@ -53,10 +63,31 @@ export default class Demo extends Component {
 					onClick={ this.clickedCanvas }
 					onLoadProgress={ this.loadProgressChanged }
 					colorBlending={ 0.6 }
-					width={ 800 }
-					height={ 800 }
+					width={ 600 }
+					height={ 600 }
+					columns={ 60 }
+					rows={ 60 }
 					sources={ Images.map(img => STATIC + 'images/' + img.name) } 
-					target={ this.state.target }/>
+					target={ this.state.target } />
+				
+				{ this.state.loadProgress >= 1 ? (
+					<p className="note">
+						Click a tile to set it as target image.
+					</p>
+				) : null }
+
+				{ this.state.target ? (
+					<div>
+						<img src={ this.getImgSrc( this.state.target ) } alt="" />
+						<p className="note">
+							Current target image.
+						</p>
+					</div>
+				) : null }
+
+				<p>
+					Images from <a href="https://unsplash.com/" target="_blank">Unsplash</a>.
+				</p>
 			</div>
 		);
 	}
